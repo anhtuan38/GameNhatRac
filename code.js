ef950b508b524358479197b0bc929a1b8767d8a4
AnhTuan <tuanphan204@gmail.com>
2022-04-15 23:28:15 +0700

    add

--- code.js
+++ code.js
@@ -2,5 +2,11 @@ for (let i = 0; i <= 13; i++)
   for (let j = 0; j <= 12; j++) {
     const div = document.createElement("div");
     div.setAttribute("class", "square");
+    div.setAttribute("data-x", i);
+    div.setAttribute("data-y", j);
     root.append(div);
   }
+
+document
+  .getElementById("start")
+  .addEventListener("click", alert("bắt đầu trò chơi"));
