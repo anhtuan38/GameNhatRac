7d7119e0307419e7bf8524bd4763f156c51abad4
AnhTuan <tuanphan204@gmail.com>
2022-04-15 23:10:31 +0700

    add

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
-for (var i = 0; i < 12; i++) {
-  document.getElementById("root").innerHTML = <div>hello</div>;
-}
+for (let i = 0; i <= 13; i++)
+  for (let j = 0; j <= 12; j++) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "square");
+    root.append(div);
+  }
