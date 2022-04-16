109b9727dbddcc98cc8241423f102559144988ae
AnhTuan <tuanphan204@gmail.com>
2022-04-16 23:50:50 +0700

    add

--- code.js
+++ code.js
@@ -1,12 +1,112 @@
-for (let i = 0; i <= 13; i++)
-  for (let j = 0; j <= 12; j++) {
-    const div = document.createElement("div");
-    div.setAttribute("class", "square");
-    div.setAttribute("data-x", i);
-    div.setAttribute("data-y", j);
-    root.append(div);
-  }
+const maxRow = 12;
+const maxColumn = 13;
+const root = document.getElementById("root");
+let indexRed = { curentX: 0, curentY: 0 };
+let arrayBlack = [
+  { x: 1, y: 5 },
+  { x: 6, y: 4 },
+  { x: 3, y: 3 },
+  { x: 4, y: 2 },
+];
 
-document
-  .getElementById("start")
-  .addEventListener("click", alert("bắt đầu trò chơi"));
+function UpdateMap() {
+  function Check(x, y) {
+    let check = false;
+    for (let i = 0; i < arrayBlack.length; i++) {
+      if (arrayBlack[i].x == x && arrayBlack[i].y == y) {
+        check = true;
+        break;
+      }
+    }
+    return check;
+  }
+  document.getElementById("childrenRoot").remove();
+  const div1 = document.createElement("div");
+  div1.setAttribute("id", "childrenRoot");
+  div1.setAttribute("class", "board-game");
+  root.append(div1);
+  for (let i = 0; i <= maxRow; i++)
+    for (let j = 0; j <= maxColumn; j++) {
+      const div = document.createElement("div");
+      div.setAttribute("class", "square");
+      div.setAttribute("data-x", i);
+      div.setAttribute("data-y", j);
+
+      if (i === indexRed.curentX && j === indexRed.curentY) {
+        const red = document.createElement("div");
+        red.setAttribute("class", "red");
+        div.append(red);
+      }
+      if (Check(i, j)) {
+        const Black = document.createElement("div");
+        Black.setAttribute("class", "black");
+        div.append(Black);
+      }
+      div1.append(div);
+    }
+}
+
+const startBtn = document.getElementById("start");
+
+function start() {
+  alert("bắt đầu trò chơi");
+  UpdateMap();
+}
+startBtn.addEventListener("click", start);
+
+const UpdateIndex = function (x, y) {
+  indexRed.curentX = x;
+  indexRed.curentY = y;
+};
+
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+const right = document.getElementById("right");
+const left = document.getElementById("left");
+
+up.addEventListener("click", (e) => {
+  if (indexRed.curentX > 0) {
+    indexRed = { ...indexRed, curentX: indexRed.curentX - 1 };
+    UpdateMap();
+  }
+});
+down.addEventListener("click", (e) => {
+  if (indexRed.curentX < maxRow) {
+    indexRed = { ...indexRed, curentX: indexRed.curentX + 1 };
+    UpdateMap();
+  }
+});
+right.addEventListener("click", (e) => {
+  if (indexRed.curentY < maxColumn) {
+    indexRed = { ...indexRed, curentY: indexRed.curentY + 1 };
+    UpdateMap();
+  }
+});
+
+left.addEventListener("click", (e) => {
+  if (indexRed.curentY > 0) {
+    indexRed = { ...indexRed, curentY: indexRed.curentY - 1 };
+    UpdateMap();
+  }
+});
+
+const pickUpTrash = () => {
+  let isIndex = arrayBlack.findIndex(
+    (item) => item.x == indexRed.curentX && item.y == indexRed.curentY
+  );
+  if (isIndex !== -1) {
+    arrayBlack.splice(isIndex, 1);
+    UpdateMap();
+    if (arrayBlack.length === 0) {
+      alert("chúc mừng bạn đã thắng");
+    }
+  }
+};
+
+const pickUpBtn = document.getElementById("pickUp");
+
+function lumRac() {
+  pickUpTrash(indexRed.curentX, indexRed.curentY);
+  UpdateMap();
+}
+pickUpBtn.addEventListener("click", lumRac);
